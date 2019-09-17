54c4cdc62c3984bf59f20a565ddb46c8e2c9ff3a
Raúl Oropeza <ral_oropeza@hotmail.com>
2019-09-16 21:13:29 -0400

    Ex1: Added the sine wave

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -23,17 +23,30 @@ let rectY;
 let rectWidth = 20;
 let rectHeight = 40;
 
-//R* The current position and size of John Lennon
+//*R The current position and size of John Lennon
 let johnImg;
 let johnX;
 let johnY;
 let johnSize;
 
-//R* The current position of the prism
-//R* Size is not needed here, I want to use its original size
+//*R The current position of the prism
+//*R Size is not needed here, I want to use its original size
 let prismImg;
 let prismX;
 let prismY;
+
+//*R The current size of the javascript logo
+let jsLogoImg;
+let jsLogoX;
+let jsLogoY;
+
+//*R The current angle, speed and scale of the sine wave
+let sineAngle;
+let sineSpeed;
+let sineScale;
+
+//R* The current offset to draw the sine wave
+let sineOffset;
 
 // preload()
 //
@@ -68,7 +81,6 @@ function setup() {
   rectX = -rectWidth/2;
   rectY = height/2;
 
-
   //*R Set the file path for John's head
   johnImg = loadImage("assets/images/john.png");
   //*R Set the size for John
@@ -78,9 +90,22 @@ function setup() {
   prismImg = loadImage("assets/images/prism.png");
   //*R Start the prism at the top of the canvas
   //*R We divide the width by 2 to start drawing in the center of the canvas
-  //*R I'm using the negative value of the images height divided by 2 to start it off the canvas
+  //*R I'm using the negative value of the image's height divided by 2 to start it off the canvas
   prismX = width/2;
   prismY = -prismImg.height/2;
+
+  //*R Set the file path for the javascript logo
+  jsLogoImg = loadImage("assets/images/jsLogo.png");
+  //*R I'm using the negative value of the image's width divided by 2 to start it off the canvas
+  //*R I want to start drawing the logo at 7/8 of the height
+  jsLogoX = -jsLogoImg.width/2;
+
+  //*R Set the values for the sine wave
+  sineSpeed = 0.03;
+  sineAngle = 0.0;
+  sineScale = 80;
+  //*R Set the offset to draw the sine wave, this is the value that the wave is going to "orbit"
+  sineOffset = 500;
 
   // We'll draw rectangles from the center
   rectMode(CENTER);
@@ -115,25 +140,32 @@ function draw() {
   // Display the square
   rect(squareX,squareY,squareSize,squareSize);
 
-  //R* Move the rectangle to the right
+  //*R Move the rectangle to the right
   rectX++;
-  //R* I see a rectangle and I want it painted black ♪♪♪
+  //*R I see a rectangle and I want it painted black ♪♪♪
   fill(0, 10);
-  //R* Display the rectangle
+  //*R Display the rectangle
   rect(rectX, rectY, rectWidth, rectHeight);
 
-  //R* Fool John into thinking that the cursor is Yoko
+  //*R Fool John into thinking that the cursor is Yoko
   johnX = mouseX;
   johnY = mouseY;
-  //R* Make John look like a ghost
+  //*R Make John look like a ghost
   tint(0, 255, 255, 10);
-  //R* Add John Lennon to the matrix
+  //*R Add John Lennon to the matrix
   image(johnImg, johnX, johnY, johnSize);
 
-  //R* Reset the tint
+  //*R Reset the tint
   tint(255);
-  //R* Move the prism towards the bottom
+  //*R Move the prism towards the bottom
   prismY++;
-  //R* Draw the prism
+  //*R Draw the prism
   image(prismImg, prismX, prismY);
+
+  //*R Move the js logo in a sine wave, the numb
+  jsLogoY = sineOffset + sin(sineAngle) * sineScale;
+  jsLogoX++;
+  sineAngle+= sineSpeed;
+  //*R Draw the js logo
+  image(jsLogoImg, jsLogoX, jsLogoY);
 }
